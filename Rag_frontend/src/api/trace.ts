c405f1e4c58466ef171772aaf8f753be90549b44
Divya Gupta <divya626343@gmail.com>
2026-08-12 00:47:38 +0530

    Delete Rag_frontend directory

--- Rag_frontend/src/api/trace.ts
+++
@@ -1,6 +0,0 @@
-import { request } from "./client";
-import type { TraceStep } from "../types";
-
-export async function getLatestTrace(sessionId: string): Promise<{ session_id: string; trace: TraceStep[] }> {
-  return request(`/trace/${sessionId}/latest`);
-}
